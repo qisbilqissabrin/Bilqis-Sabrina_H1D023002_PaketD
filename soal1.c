56708bf8a6cb8108480f1249b3ee24e118135017
qisbilqissabrin <162335353+qisbilqissabrin@users.noreply.github.com>
2024-06-06 14:01:31 +0700

    Create soal1.c
    Signed-off-by: qisbilqissabrin <162335353+qisbilqissabrin@users.noreply.github.com>

---
+++ soal1.c
@@ -0,0 +1,28 @@
+typedef struct node *alamat {
+    int data;
+     link;
+}
+
+
+void updateIP(int value, *x){
+if (first == NULL){
+    first = last=baru 
+}else{
+    baru-> next = first;
+    first->prev = baru;
+    first=baru;
+}
+}
+ int main(){
+printf("Sebelum IP diperbarui:");
+printf ("NIM:");
+printf("IP:");
+return 0;
+ }
+
+ void cetak(){
+    printf ("Setelah IP diperbarui:\n");
+
+
+    return 0;
+ }
